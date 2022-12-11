d9755ea85d051df6cd33e6bbfdbe772e1fd64990
Agnes <almoha@gmail.com>
2022-12-11 09:29:04 +0100

    ...

--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -10,9 +10,11 @@ const Profil = () => {
 	return (
 		<div className="flex flex-col w-full h-screen">
 			<Navigation />
-			<main className="flex-1 flex  items-center justify-center">
-				<h1>Welcome</h1>
-				<p>This is the Home Page, if you can see this you're logged in.</p>
+			<main className="flex-1 flex flex-col items-center justify-center">
+				<h1 className="text-5xl mb-10">Welcome</h1>
+				<p className="text-2xl">
+					This is the Home Page, if you can see this you're logged in.
+				</p>
 			</main>
 			<Footer />
 		</div>
